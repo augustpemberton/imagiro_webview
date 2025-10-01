3cdc43dcf806f4f58494924492236c06218b78b1
August Pemberton <augustmpemberton@gmail.com>
2025-10-01 17:28:59 +0100

    fix header includes

--- imagiro_webview.h
+++ imagiro_webview.h
@@ -3,6 +3,8 @@
 #include "src/attachment/AuthAttachment.h"
 #include "src/attachment/util/BufferUtil.h"
 #include "src/AssetServer/BinaryDataAssetServer.h"
+#include "src/connection/web/WebProcessor.h"
+#include "src/connection/web/WebUIPluginEditor.h"
 
 #if JUCE_WINDOWS or JUCE_MAC
 #include "src/connection/web/WebUIPluginEditor.h"
